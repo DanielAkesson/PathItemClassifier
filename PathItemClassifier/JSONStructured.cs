3aa4ce4d5bd65562c2425779e38a154356099c38
Daniel Åkesson <argilstudio@gmail.com>
2020-02-28 01:00:41 +0100

    Add project files.

---
+++ PathItemClassifier/JSONStructured.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathItemClassifier
+{
+    //Public Stash Tabs Query
+    struct POEAPI_StashResponseJSON
+    {
+        public string next_change_id;
+        public POEAPI_StashJSON[] stashes;
+    }
+    struct POEAPI_StashJSON
+    {
+        public string id;
+        public string league;
+        public POEAPI_ItemJSON[] items;
+    }
+    struct POEAPI_ItemJSON
+    {
+        public string icon;
+        public string league;
+        public string id;
+        public POEAPI_SocketJSON[] sockets;
+        public string name;
+        public string typeLine;
+        public bool identified;
+        public int ilvl;
+        public string note;
+        public bool corrupted;
+        public POEAPI_PropertiesJSON[] properties;
+        public POEAPI_RequirmentsJSON[] requirements;
+        public string[] explicitMods;
+        public POEAPI_ExtendedJSON extended;
+    }
+    struct POEAPI_SocketJSON
+    {
+        public int group;
+        public string attr;
+        public string sColour;
+    }
+    struct POEAPI_PropertiesJSON
+    {
+        public string name;
+        public string[][] values;
+        public int type;
+    }
+    struct POEAPI_RequirmentsJSON
+    {
+        public string name;
+        public string[][] values;
+    }
+    struct POEAPI_ExtendedJSON
+    {
+        public string category;
+        public string[] subcategories;
+    }
+}
---
+++ PathItemClassifier/POEAPI_Wraper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PathItemClassifier
+{
+    class POEAPI_Wrapper
+    {
+        const string POEAPI_StashQuery = "http://api.pathofexile.com/public-stash-tabs/?id=$ID$";
+
+        public static POEAPI_StashResponseJSON POEAPIStashQueryById(string id = "0")
+        {
+            string request = POEAPI_StashQuery.Replace("$ID$", id);
+            string jsonResponse = CompressedHttpGet(request);
+            return JsonConvert.DeserializeObject<POEAPI_StashResponseJSON>(jsonResponse);
+        }
+        public static POEAPI_StashResponseJSON[] POEAPIAllStashQuery()
+        {
+            List<POEAPI_StashResponseJSON> stashes = new List<POEAPI_StashResponseJSON>();
+            stashes.Add(POEAPIStashQueryById());
+            for (int i = 0; i < 100; i++)
+            {
+                stashes.Add(POEAPIStashQueryById(stashes[i].next_change_id));
+                Console.WriteLine(stashes[i].next_change_id);
+            }
+                
+            return stashes.ToArray();
+        }
+        private static string CompressedHttpGet(string URI)
+        {
+            System.Net.HttpWebRequest.DefaultWebProxy = null;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+            //request.AutomaticDecompression = DecompressionMethods.GZip;
+            string html = "";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream)) { html = reader.ReadToEnd(); }
+            return html;
+        }
+    }
+}
